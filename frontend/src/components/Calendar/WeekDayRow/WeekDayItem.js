5b7cbb9852dd82bdb42561536a7e082f8afbac27
Eduard <EduardTurdalijev@gmail.com>
2020-02-08 13:24:35 +0200

    [Frontend] Add calendar styles

--- frontend/src/components/Calendar/WeekDayRow/WeekDayItem.js
+++ frontend/src/components/Calendar/WeekDayRow/WeekDayItem.js
@@ -1,11 +1,10 @@
 import styled from "styled-components";
 
 export default styled.span`
-    display: block;
     font-size: 14px;
     text-align: center;
     padding: 4px;
-    height:  calc(100vw / 7);
+    height:  calc(99vw / 7);
     width:  calc(100% / 7);
     align-items: center;
     display: flex;
--- frontend/src/components/Calendar/WeekDayRow/Wrapper.js
+++ frontend/src/components/Calendar/WeekDayRow/Wrapper.js
@@ -3,4 +3,6 @@ import styled from "styled-components";
 export default styled.div`
     display: flex;
     width: 99vw;
+    max-width: 640px;
+    max-height: 640px;
 `;
